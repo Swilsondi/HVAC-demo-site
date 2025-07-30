aff173e7cd8161d991dc80806e4fc4e9bb1c5bc8
Swilsondi <165120193+Swilsondi@users.noreply.github.com>
2025-07-29 23:25:16 -0400

    app change

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-// Services and Contact will be added next
+import { Navbar } from "./components/layout/Navbar";
+import { Hero } from "./components/sections/Hero";
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-gray-50">
-        <Navbar />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            {/* <Route path="/services" element={<Services />} /> */}
-            {/* <Route path="/contact" element={<Contact />} /> */}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="bg-black text-white min-h-screen">
+      <Navbar />
+      <main className="pt-20"> {/* Top padding to offset fixed navbar */}
+        <Hero />
+        {/* Add more sections here like Features, Services, Demos, etc. */}
+      </main>
+    </div>
   );
 }
 
